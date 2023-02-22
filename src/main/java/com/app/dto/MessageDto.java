6c4f284485040b8cd852a305fb66729f0cd780a9
zengetsuichi <74659025+zengetsuichi@users.noreply.github.com>
2023-02-22 18:02:15 +0100

    MessageDto added to DTOs

---
+++ src/main/java/com/app/dto/MessageDto.java
@@ -0,0 +1,18 @@
+package com.app.dto;
+
+import lombok.*;
+
+
+
+@ToString
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class MessageDto {
+
+    private String from;
+    private String to;
+    private String subject;
+    private String text;
+}
