e2265b80a3f121233a9d41b4f1a8f0a8ac2d134a
MG <g.maciek55@gmail.com>
2023-02-22 16:58:50 +0100

    Dodanie adnotacji @NoArgsContructor

--- src/main/java/com/app/command/GenerateExamCommand.java
+++ src/main/java/com/app/command/GenerateExamCommand.java
@@ -2,13 +2,14 @@ package com.app.command;
 
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 import java.util.List;
 
 @Data
-@Builder
 @ToString
+@NoArgsConstructor
 public class GenerateExamCommand {
     private String email;
     private Long examNumber;
